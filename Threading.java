4d21eb57bdf06f679ddc8db490cabad30de9f2bf
Harsh Shukla <harshshukla24112000@gmail.com>
2026-07-03 13:15:53 +0530

    thread optimization

--- Threading.java
+++ Threading.java
@@ -2,8 +2,14 @@ class outer extends Thread
 {
     public void run()
     {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 50; i++) {
              System.out.println("In outer show");
+             try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
         }
        
     }
@@ -12,7 +18,7 @@ class outer extends Thread
 class inner extends Thread {
      public void run()
     {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 50; i++) {
              System.out.println("In inner show");
         }
     }
@@ -24,6 +30,13 @@ class Threading            //in every thread have a run method
         outer obj1 = new outer();
         inner obj2 = new inner();
         obj1.start();
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
         obj2.start();
     }
 }
